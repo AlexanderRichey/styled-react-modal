cee0f3843444629523daa783877ff7b841b35278
Alexander Richey <alexander.richey@gmail.com>
2018-09-03 16:33:26 -0400

    write tests for modal component

--- tests/Modal.test.jsx
+++ tests/Modal.test.jsx
@@ -12,13 +12,86 @@ describe('<Modal />', () => {
 
   const Content = () => <span>content</span>
 
-  it('renders without crashing', () => {
+  it('calls beforeOpen before it opens', () => {
+    const mockCb = jest.fn()
+    const mockNoCallCb = jest.fn()
     const wrapper = shallow(
-      <Modal>
+      <Modal
+        beforeOpen={mockCb}
+        beforeClose={mockNoCallCb}>
         <Content />
       </Modal>
     )
 
-    expect(wrapper)
+    wrapper.setProps({ isOpen: true })
+
+    expect(mockCb.mock.calls.length).toBe(1)
+    expect(mockNoCallCb.mock.calls.length).toBe(0)
+  })
+
+  it('calls afterOpen after it opens', () => {
+    const mockCb = jest.fn()
+    const mockNoCallCb = jest.fn()
+    const wrapper = shallow(
+      <Modal
+        afterOpen={mockCb}
+        afterClose={mockNoCallCb}>
+        <Content />
+      </Modal>
+    )
+
+    wrapper.setProps({ isOpen: true })
+
+    expect(mockCb.mock.calls.length).toBe(1)
+    expect(mockNoCallCb.mock.calls.length).toBe(0)
+  })
+
+  it('calls beforeClose before it closes', () => {
+    const openCb = jest.fn()
+    const mockCb = jest.fn()
+    const wrapper = shallow(
+      <Modal
+        beforeOpen={openCb}
+        afterOpen={openCb}
+        beforeClose={mockCb}>
+        <Content />
+      </Modal>
+    )
+
+    wrapper.setProps({ isOpen: true })
+    wrapper.setProps({ isOpen: false })
+
+    expect(openCb.mock.calls.length).toBe(2)
+    expect(mockCb.mock.calls.length).toBe(1)
+  })
+
+  it('calls afterClose after it closes', () => {
+    const openCb = jest.fn()
+    const mockCb = jest.fn()
+    const wrapper = shallow(
+      <Modal
+        beforeOpen={openCb}
+        afterOpen={openCb}
+        afterClose={mockCb}>
+        <Content />
+      </Modal>
+    )
+
+    wrapper.setProps({ isOpen: true })
+    wrapper.setProps({ isOpen: false })
+
+    expect(openCb.mock.calls.length).toBe(2)
+    expect(mockCb.mock.calls.length).toBe(1)
+  })
+})
+
+describe('Modal.styled()', () => {
+  it('returns to a Modal instance', () => {
+    const StyledModal = Modal.styled`
+      background-color: green;
+    `
+    const wrapper = shallow(<StyledModal />)
+
+    expect(wrapper.type()).toBe(Modal)
   })
 })
