b04330e5709e6778784106b1bad7d06ce03e9816
Wenny Hidayat <wenny.h@live.com>
2020-08-25 09:56:52 +1000

    Fixes to base width and height
    This `100vh` config is not compatible with Safari mobile - it gets obscured by the dynamic toolbar and nav bar when using viewports as base height https://mobile.twitter.com/simevidas/status/779161023790116865 https://www.eventbrite.com/engineering/mobile-safari-why/
    
    Proposed to change it to base height and width to 100% so that the modal size changes with screen size instead of static viewports.

--- src/baseStyles.jsx
+++ src/baseStyles.jsx
@@ -5,8 +5,8 @@ export const BaseModalBackground = styled.div`
   position: fixed;
   top: 0;
   left: 0;
-  width: 100vw;
-  height: 100vh;
+  width: 100%;
+  height: 100%;
   z-index: 30;
   background-color: rgba(0, 0, 0, 0.5);
   align-items: center;
