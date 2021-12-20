1d9a49a6c268edeb5c1cfd4c5672302562ec0f4b
Alexander Richey <alexander.richey@gmail.com>
2021-12-19 19:03:24 -0500

    try rollup

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,8 @@
-import { BaseModalBackground } from './baseStyles'
-import ModalProvider from './ModalProvider'
-import Modal from './Modal'
+import { BaseModalBackground } from "./baseStyles";
+import ModalProvider from "./ModalProvider";
+import Modal from "./Modal";
 
-export { ModalProvider, BaseModalBackground }
-export default Modal
+Modal.ModalProvider = ModalProvider;
+Modal.BaseModalBackground = BaseModalBackground;
+
+export default Modal;
