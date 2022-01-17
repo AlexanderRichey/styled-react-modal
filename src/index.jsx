6698d25db75dbbb5ff9216bf96b855dd35c7ad11
Alexander Richey <alexander.richey@gmail.com>
2022-01-16 18:07:42 -0800

    use 'named' exports

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,4 @@ import { BaseModalBackground } from "./baseStyles";
 import ModalProvider from "./ModalProvider";
 import Modal from "./Modal";
 
-Modal.ModalProvider = ModalProvider;
-Modal.BaseModalBackground = BaseModalBackground;
-
-export default Modal;
+export { Modal as default, ModalProvider, BaseModalBackground };
